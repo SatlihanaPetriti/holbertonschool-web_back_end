bcd0ef64e670276507a66fa6d63f1732e4eead02
satlihana_p <satlihanapetriti@gmail.com>
2026-01-21 00:46:18 +0100

    comments

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -2,15 +2,17 @@ import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-
+//new object
   const fullBudget = {
+    //spread operator
     ...budget,
-
+    // object method
     getIncomeInDollars(income) {
       return `$${income}`;
     },
-
+    // object method
     getIncomeInEuros(income) {
+              // template literal
       return `${income} euros`;
     },
   };
