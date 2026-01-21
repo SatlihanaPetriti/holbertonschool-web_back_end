6c4b7356e29f38be7919ad29ba2146022decb167
satlihana_p <satlihanapetriti@gmail.com>
2026-01-21 23:24:26 +0100

    11

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) resolve({ status: 200, body: 'Success' });
-    else reject(Error('The fake API is not working currently'));
+    reject(Error('The fake API is not working currently'));
   });
 }
