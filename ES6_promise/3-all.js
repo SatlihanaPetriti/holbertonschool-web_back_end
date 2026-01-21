3278dc8f4606864a407dbfc5a5db56501e2013bf
satlihana_p <satlihanapetriti@gmail.com>
2026-01-21 23:39:57 +0100

    3-main

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,7 +1,11 @@
-// import {uploadPhoto, createUser} from "./utils.js";
-// function handleProfilesign up(){
-//     return Promise.all ([uploadphoto(), createuser()])
-//     .then((Response)=>{
+import { createUser, uploadPhoto } from './utils.js';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`
         
-//     })
-// }
+      );
+    })
+    .catch(() => console.log('Signup system offline'));
+}
