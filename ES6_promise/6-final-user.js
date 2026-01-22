d47de6241f9491f37b7311d552ae7aaa653dac76
satlihana_p <satlihanapetriti@gmail.com>
2026-01-22 18:51:03 +0100

    final user

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup(firstName, lastName, filename) {
 
   return Promise.allSettled(promises)
     .then((results) => results.map((result) => (result.status === 'fulfilled'
-      //if..else
+      //if ?..else:
       ? { status: result.status, value: result.value }
       : { status: result.status, value: result.reason.toString() })))//
     .catch((error) => error);
