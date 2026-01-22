940f75e91fa1c6c9deae6af744085a9b829bff6f
satlihana_p <satlihanapetriti@gmail.com>
2026-01-22 15:38:04 +0100

    main lint

--- ES6_promise/100-main.js
+++ ES6_promise/100-main.js
@@ -1,7 +1,8 @@
-import asyncUploadUser from "./100-await";
-const test = async () => {
-    const value = await asyncUploadUser();
-    console.log(value);
+import asyncUploadUser from './100-await';
+
+const run = async () => {
+  const value = await asyncUploadUser();
+  console.log(value);
 };
 
-test();
+run();
