5bc999e565f4e08568c98af148dae35680c6c6e0
satlihana_p <satlihanapetriti@gmail.com>
2026-01-27 16:44:08 +0100

    map and filter

--- ES6_data_manipulation/4-main.js
+++ ES6_data_manipulation/4-main.js
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 import getListStudents from "./0-get_list_students.js";
 import updateStudentGradeByCity from "./4-update_grade_by_city.js";
 
--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,25 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeObj = newGrades.find(
-        grade => grade.studentId === student.id
-      );
-
-      return {
-        ...student,
-        grade: gradeObj ? gradeObj.grade : 'N/A',
-      };
-    });
+/*eslint-disable*/
+function updateStudentGradeByCity(students, city, newGrades) {
+  const cityStudents = students.filter(student => student.location === city);
+  const result = cityStudents.map(student => {
+    const studentCopy = { ...student };
+    let foundGrade = false;
+    for (const gradeObj of newGrades) {
+      if (gradeObj.studentId === student.id) {
+        studentCopy.grade = gradeObj.grade;
+        foundGrade = true;
+        break;
+      }
+    }
+    
+    if (!foundGrade) {
+      studentCopy.grade = 'N/A';
+    }
+    
+    return studentCopy;
+  });
+  
+  return result;
 }
+
+export default updateStudentGradeByCity;
