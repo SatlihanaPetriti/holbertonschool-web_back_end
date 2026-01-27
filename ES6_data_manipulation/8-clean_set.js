5a77cdd96307cee445d3ff5943054c5d91a2c984
satlihana_p <satlihanapetriti@gmail.com>
2026-01-27 17:37:15 +0100

    groceries list

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,13 @@ export default function cleanSet(set, startString){
     if (!startString || typeof startString !== 'string') return '';
   
   return Array.from(set)
+//typeof value === 'string' → Only keep string values
+// value.startsWith(startString) → Only keep strings that begin with 
+//startString = 'bon'
     .filter(value => typeof value === 'string' && value.startsWith(startString))
+// Removes the startString from the beginning of each string:
+// value.slice(startString.length) → Takes substring starting from position startString.length
     .map(value => value.slice(startString.length))
+//Combines all array elements into a single string separated by -:
     .join('-');
 }
