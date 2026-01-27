a4e3950956c858caafa3c8490d510236b21baf8c
satlihana_p <satlihanapetriti@gmail.com>
2026-01-27 16:58:57 +0100

    8 typed array

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,17 @@
+/*eslint-disable*/
+function createInt8TypedArray(length, position, value) {
+  if (position < 0) {
+    throw new Error('Position outside range');
+  }
+  
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+  int8View[position] = value;
+
+  return new DataView(buffer);
+}
+
+export default createInt8TypedArray;
