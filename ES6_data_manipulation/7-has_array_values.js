c805efca8a4ee070872fd7fe44a839f2cdaf56e7
satlihana_p <satlihanapetriti@gmail.com>
2026-01-27 17:14:46 +0100

    set 7

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,11 @@
+/*eslint-disable*/
+function hasValuesFromArray(set, array){
+    for (const element of array) {
+    if (!set.has(element)) {
+      return false;
+    }
+  }
+  return true;
+
+}
+export default hasValuesFromArray;
