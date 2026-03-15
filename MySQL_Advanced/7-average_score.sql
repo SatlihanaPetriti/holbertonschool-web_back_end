453500bcfc9bcb0a6443b5e05c3cbf4534c3c474
satlihana_p <satlihanapetriti@gmail.com>
2026-03-15 14:36:14 +0100

    space

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,14 +1,19 @@
---script average score
+-- script average score
 DELIMITER / /
 
-CREATE PROCEDURE ComputeAverageScoreForUser( IN user_id INT) BEGIN DECLARE avg_score FLOAT;
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
+BEGIN
+    DECLARE avg_score DECIMAL(10, 2);
 
-SET avg_score = (
-SELECT  AVG(score)
-FROM corrections AS C
-WHERE C.user_id=user_id); UPDATE users 
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE corrections.user_id = user_id;
 
-SET average_score = avg_score
-WHERE id=user_id;
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
 END //
+
 DELIMITER;
