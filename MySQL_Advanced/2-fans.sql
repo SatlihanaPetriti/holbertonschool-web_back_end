8657876eecc4286bb158926bd5077855593c396a
satlihana_p <satlihanapetriti@gmail.com>
2026-03-15 13:22:22 +0100

    fans

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,9 +1,5 @@
-SELECT 
-    origin, 
-    SUM(fans) as nb_fans
-FROM 
-    metal_bands
-GROUP BY 
-    origin
-ORDER BY 
-    nb_fans DESC;
+--script fans
+SELECT origin, SUM(fans) as nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
